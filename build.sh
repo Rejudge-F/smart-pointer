8f9317b19904c14ba97c91e3edbfa67994549493
Zephyr <brave_zephyr@163.com>
2019-11-27 13:31:36 +0000

    build shell

---
+++ build.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+CC=g++
+CFLAGS=-std=c++11 
+FILENAME=`ls *.cpp`
+
+for FILE in "$FILENAME"; do
+    #statements
+    "$CC" "$CFLAGS" -c "$FILE" -o "${FILE%.*}".o
+done
+
+OBJECTS=`ls *.o`
+if [ -d "bin" ]; then
+    #statements
+    "$CC" "$CFLAGS" "$OBJECTS" -o bin/App
+else
+    echo "generate bin directory"
+    mkdir bin 
+    "$CC" "$CFLAGS" "$OBJECTS" -o bin/App
+fi
+
