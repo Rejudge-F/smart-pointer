1830c81c81dcfc9cab331ba108100ca384972147
Zephyr <brave_zephyr@163.com>
2019-11-27 13:30:58 +0000

    add shared_count class

--- smart_ptr.h
+++ smart_ptr.h
@@ -2,12 +2,25 @@
 #define SMART_PTR_H
 #include <utility>
 
+class shared_count {
+public:
+    shared_count() 
+        : count_(1)
+    {}
+    ~shared_count() {}
+    void add_count() { count_++; }
+    long reduce_count() {return --count_; }
+    long get_count() {return count_;}
+private:
+    long count_;
+};
+
 template <typename T>
 class smart_ptr 
 {
 private:
 	T* ptr_;
-	
+    shared_count* shared_count_;	
 //	// noncopyable 
 //	smart_ptr(const smart_ptr& ptr) 
 //		= delete;
@@ -16,36 +29,68 @@ private:
 
 public:
 	explicit smart_ptr(T* ptr = nullptr)
-		: ptr_(ptr) {}
+        : ptr_(ptr) {
+            if(ptr) {
+                shared_count_ = 
+                    new shared_count();
+            }
+        }
 	virtual ~smart_ptr() {
-		delete ptr_;
+        if (ptr_ && !shared_count_->reduce_count()) {
+            delete shared_count_;
+            delete ptr_;
+        }
 	}
+
+    template <typename U>
+    friend class smart_ptr;
     
 	// get real ptr 
 	T* get() const {
 		return ptr_;
 	}
 
+    operator bool() const {return ptr_;}
 	T& operator*() const {return *ptr_;}
 	T* operator->() const {return ptr_;}
+    template <typename U>
+    smart_ptr(const smart_ptr<U> &other) {
+        ptr_ = other.ptr_;
+        if (ptr_) {
+            other.shared_count_->add_count();
+            shared_count_ = 
+                other.shared_count_;
+        }
+    }
 
 	template <typename U>
-	smart_ptr(smart_ptr<U>&& ptr) {ptr_ = ptr.release();}
+	smart_ptr(smart_ptr<U>&& other) {
+        ptr_ = other.ptr_;
+        if (ptr_) {
+            shared_count_ = 
+                other.shared_count_;
+            other.ptr_ = nullptr;
+        }
+    }
 	smart_ptr& operator=(smart_ptr rhs) {
 		rhs.swap(*this);
 		return *this;
 	}
 
-	T* release() {
-		T* ptr = ptr_;
-		ptr_ = nullptr;
-		return ptr;
-	}
+    long use_count() const {
+        if (ptr_) {
+            return shared_count_
+                ->get_count();
+        } else {
+            return 0;
+        }
+    }
 
 	void swap(smart_ptr &rhs) {
 		using std::swap;
 		swap(ptr_, rhs.ptr_);
-	}operator bool() const {return ptr_;}
+        swap(shared_count_, rhs.shared_count_);
+	}
 };
 
 #endif /* SMART_PTR_H */	
