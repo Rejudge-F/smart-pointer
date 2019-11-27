7647027ba332c64530166597253cacf67a485554
Zephyr <brave_zephyr@163.com>
2019-11-27 13:31:20 +0000

    test shared_count is work

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "smart_ptr.h"
+
+class shape {
+public:
+  virtual ~shape() {}
+};
+
+class circle : public shape {
+public:
+  ~circle() { puts("~circle()"); }
+};
+
+int main()
+{
+  smart_ptr<circle> ptr1(new circle());
+  printf("use count of ptr1 is %ld\n",
+         ptr1.use_count());
+  smart_ptr<shape> ptr2;
+  printf("use count of ptr2 was %ld\n",
+         ptr2.use_count());
+  ptr2 = ptr1;
+  printf("use count of ptr2 is now %ld\n",
+         ptr2.use_count());
+  if (ptr1) {
+    puts("ptr1 is not empty");
+  }
+}
